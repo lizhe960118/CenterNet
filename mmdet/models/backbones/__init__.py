1a33c198ec55366d52b9fb5c4282f2ac6090a756
lizhe_dgnet <lizhe@ubuntu14x64.example.com>
2019-08-20 14:47:46 +0800

    add resnet_dcn

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -8,5 +8,6 @@ from .hrnet2 import HRNet2
 from .hrnet3 import HRNet3
 from .hrnet4 import HRNet4
 from .dla2 import DLA2
+from .resnet_dcn import ResNetDCN
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'HourglassNet', 'DLA', 'HRNet2', 'HRNet3', 'HRNet4', 'DLA2']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet', 'HourglassNet', 'DLA', 'HRNet2', 'HRNet3', 'HRNet4', 'DLA2', 'ResNetDCN']
